1e74cac8c52b0b6fc4cb7cd225352d0265632ff6
badikirwan <badikirwan@gmail.com>
2018-11-06 03:46:54 +0700

    Change to Bottom Navigation

--- app/src/main/java/com/badikirwan/dicoding/footballmatchschedule/view/main/MainActivity.kt
+++ app/src/main/java/com/badikirwan/dicoding/footballmatchschedule/view/main/MainActivity.kt
@@ -3,7 +3,12 @@ package com.badikirwan.dicoding.footballmatchschedule.view.main
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.badikirwan.dicoding.footballmatchschedule.R
+import com.badikirwan.dicoding.footballmatchschedule.R.id.menu_last_match
+import com.badikirwan.dicoding.footballmatchschedule.R.id.menu_next_match
+import com.badikirwan.dicoding.footballmatchschedule.R.id.menu_favorite
 import com.badikirwan.dicoding.footballmatchschedule.adapter.FragmentAdapter
+import com.badikirwan.dicoding.footballmatchschedule.fragment.LastMatchFragment
+import com.badikirwan.dicoding.footballmatchschedule.fragment.NextMatchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +17,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewpager_main.adapter = FragmentAdapter(supportFragmentManager)
-        tabs_main.setupWithViewPager(viewpager_main)
+        //viewpager_main.adapter = FragmentAdapter(supportFragmentManager)
+        //tabs_main.setupWithViewPager(viewpager_main)
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                menu_last_match -> {
+                    loadLastMatchFragment(savedInstanceState)
+                }
+                menu_next_match -> {
+                    loadNextMatchFragment(savedInstanceState)
+                }
+                menu_favorite -> {
 
+                }
+            }
+            true
+        }
+
+        bottom_navigation.selectedItemId = menu_last_match
+    }
+
+    private fun loadLastMatchFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, LastMatchFragment(), LastMatchFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+
+    private fun loadNextMatchFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, NextMatchFragment(), NextMatchFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
